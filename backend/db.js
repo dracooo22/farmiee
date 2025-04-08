67432cf9134bb44153300650c55adeada26efba0
dracooo22 <b22cs063@mace.ac.in>
2025-04-08 14:18:14 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb+srv://b22cs063:b22cs063@cluster0.jurfpzv.mongodb.net/farmie?retryWrites=true&w=majority&appName=Cluster0';
+
 const mongoDB = async () => {
   try {
     await mongoose.connect(mongoURI, {
-      serverSelectionTimeoutMS: 5000, // Wait 5s before failing
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      maxPoolSize: 10, // Limit the number of connections in the pool
+      serverSelectionTimeoutMS: 5000, // Wait 5 seconds before failing
     });
 
     console.log('✅ MongoDB connected successfully');
